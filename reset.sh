69d7e36cf28fb7d385b518f837f48f1bbadc9f30
Erjan Gavalji <erjan.gavalji@gmail.com>
2025-01-06 17:02:59 +0200

    Add the secret file. Rename the mongo deployment. Add a reset script

---
+++ reset.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+kubeconfig=$1
+if [ -z "$kubeconfig" ]; then
+	echo "Usage: $0 <kubeconfig>"
+	exit 1
+fi
+
+export KUBECONFIG=$KUBECONFIG
+
+
+function show_progress() {
+	total=$1
+	if [ -z "$total" ]; then
+		total=10
+	fi
+	time=$2
+	if [ -z "$time" ]; then
+		time=3
+	fi
+	for i in $(seq 1 ${total}); do
+		dots=$(printf '.%.0s' $(seq 1 $i))
+		echo -ne "\r${dots} ${i}/${total}"
+		sleep 3
+	done
+	echo ""
+}
+
+echo "Deleting the real service..."
+kubectl delete -f ./dbs.yml
+echo "Give the system some time to delete the real service..."
+show_progess 20 3
+echo ""
+
+echo "Deleting the pvc and pv..."
+pvName=$(kubectl get pvc my-pvc -o jsonpath='{.spec.volumeName}')
+echo "pvName: ${pvName}"
+kubectl delete pvc my-pvc
+kubectl delete pv ${pvName}
+echo "Give the system some time to delete the pv and pvc..."
+show_progress 10 3
+echo ""
+
+echo "Deleting the storage class..."
+kubectl delete -f ./storage-class.yml
+
+echo "Deleting the secret..."
+kubectl delete secret my-luks-secret -n csi-encrypt-keys
+
+echo "Deleting the namespaces..."
+kubectl delete -f ./namespaces.yml
