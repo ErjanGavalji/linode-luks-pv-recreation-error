4ab95719d17cb10ae7a8ccfef95e74564fe9bc29
Erjan Gavalji <erjan.gavalji@gmail.com>
2025-02-03 09:25:46 +0200

    Simplify the "standard" creation demo

--- all-steps.sh
+++ all-steps.sh
@@ -43,45 +43,9 @@ echo ""
 #echo ""
 
 echo "Creating the real service..."
-./step6-create-real-service.sh $kubeConfig
+./step4-create-real-service.sh $kubeConfig
 echo "Give the system some time to create the real service..."
 show_progress 60 3
-echo ""
-
-VOLUME_DETAILS=$(./step5-get-volume-details.sh $kubeConfig)
-# VOLUME_DETAILS has each item in a new line, having the label separated from the value by a colon:
-pvName=$(echo "$VOLUME_DETAILS" | sed -n 's/^pvName://p')
-volumeHandle=$(echo "$VOLUME_DETAILS" | sed -n 's/^volumeHandle://p')
-echo "pvName: ${pvName}"
-echo "volumeHandle: ${volumeHandle}"
-echo ""
-
-echo "Deleting the real service..."
-./step7-delete-real-service.sh $kubeConfig
-echo "Give the system some time to delete the real service..."
-show_progress 10 3
-echo""
-
-echo "Deleting the pvc..."
-./step8-delete-pvc.sh $kubeConfig
-echo "Give the system some time to delete the pvc..."
-show_progress 10 3
-echo ""
-
-echo "Deleting the pv..."
-./step9-delete-pv.sh $kubeConfig $pvName
-echo "Give the system some time to delete the pv..."
-show_progress 10 3
-echo ""
-
-./step10-recreate-pv.sh $kubeConfig $pvName $volumeHandle
-echo "Give the system some time to recreate the pv..."
-show_progress 20 3
-echo ""
-
-./step11-recreate-real-service.sh $kubeConfig
-echo "Give the system some time to attempt recreating the real service..."
-show_progress 10 3
 echo ""
 
 echo ""
@@ -91,12 +55,6 @@ echo ""
 echo ""
 echo "================================================================================"
 kubectl describe pvc mongodb-data-ze-mongodb-0 -n my-namespace
-echo ""
-echo ""
-echo ""
-echo ""
-echo "================================================================================"
-kubectl describe pv ${pvName}
 echo ""
 echo ""
 echo ""
